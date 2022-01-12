1a7314ded7581cad4e46f3d23e982207e0246bd6
Dercraker <antoine.capitain@gmail.com>
2022-01-12 16:05:09 +0100

    Create Position

---
+++ Tetris/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Position
+    {
+        public int row { get => row; set => row = value;}
+        public int column { get => column; set => row = value;}
+
+
+        public Position(int r, int c)
+        {
+            this.row = r;
+            this.column = c;
+        }
+    }
+}
