7a166537b9423bfd0ef40f8810201e0a4aacf0ac
Dercraker <antoine.capitain@gmail.com>
2022-01-12 16:38:40 +0100

    Tetramino

--- Tetris/Tetramino.cs
+++ Tetris/Tetramino.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace Tetris
 {
-    internal class Tetramino
+    public abstract class Tetramino
     {
+        public Position[][] boxRotation { get; set; }
+        public Position spawnPoint { get; set; }
+        public int tetraminoId {  get; set; }
+        public int rotate { get; set; }
+        public Position offSet { get; set; }
+
+        public Tetramino()
+        {
+            offSet = new Position(spawnPoint.row, spawnPoint.column);
+        }
+
+        public IEnumerable<Position> positionsOfRotation()
+        {
+            foreach (Position p in boxRotation[rotate])
+            {
+                yield return new Position(p.row + offSet.row, p.column + offSet.column);
+            }
+        }
+
+        public void NextRotate()
+        {
+            rotate = (rotate + 1) % boxRotation.Length;
+        }
+        public void PrevRotate()
+        {
+            if (rotate == 0)
+            {
+                rotate = boxRotation.Length - 1;
+            }
+            else
+            {
+                rotate--;
+            }
+        }
+        public void MoveTetramino(int r, int c)
+        {
+            offSet.row = r;
+            offSet.column = c;
+        }
+        public void reset()
+        {
+            rotate = 0;
+            offSet = new Position(spawnPoint.row, spawnPoint.column);
+        }
+
     }
 }
