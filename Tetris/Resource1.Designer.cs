898b1463f0f0c64cc9f60b523fac1eb75ed9e417
Dercraker <antoine.capitain@gmail.com>
2022-02-02 16:05:56 +0100

    New Ressources test

--- Tetris/Resource1.Designer.cs
+++ Tetris/Resource1.Designer.cs
@@ -123,6 +123,16 @@ namespace Tetris {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
+        public static byte[] GameOverBackground {
+            get {
+                object obj = ResourceManager.GetObject("GameOverBackground", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
         public static byte[] GameOverBG {
             get {
                 object obj = ResourceManager.GetObject("GameOverBG", resourceCulture);
