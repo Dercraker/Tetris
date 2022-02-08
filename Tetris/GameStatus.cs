22d4ad4af724dbd1e194a6997831da52fde4b3d2
TimBruyere <timothe.bruyere@ynov.com>
2022-02-08 18:06:51 +0100

    New class Scores

--- Tetris/GameStatus.cs
+++ Tetris/GameStatus.cs
@@ -23,9 +23,7 @@ namespace Tetris
                 currentTetramino.reset();
             }
         }
-        public BitmapImage ToImage { get; set; }
-        public GameGird gameGrid { get; }
-        public int Score { get; private set; }
+        public GameGird gameGrid { get; }      
         public WaitingLine waitingLine { get; }
         public bool gameOver { get; private set; }
         public int GameSpeed { get; set; }
@@ -33,20 +31,18 @@ namespace Tetris
         public int time { get; private set; }
         public int lastScoreTime { get; set; }
         public int combos { get; set; }
-
+        public Scores scores { get; set; }
 
         private DispatcherTimer Timer;
 
         public GameStatus()
         {
-            Score = 0;
             GameSpeed = 400;
             SpeedLevel = 1;
-            lastScoreTime = 0;
-            combos = 1;
             gameGrid = new GameGird(22, 10);
             waitingLine = new WaitingLine();
             currentTetramino = waitingLine.UpdateTetramino();
+            scores = new Scores();
         }
 
         public void SetTimer()
@@ -127,14 +123,15 @@ namespace Tetris
             }
             return false;
         }
+
         private void PlaceItem()
         {
             foreach (Position p in currentTetramino.positionsOfRotation())
             {
                 gameGrid[p.row, p.column] = currentTetramino.tetraminoId;
             }
-            Score += bonusScore(gameGrid.ClearGrid());
-            NewGameSpeed(Score);
+            scores.score += scores.bonusScore(gameGrid.ClearGrid());
+            NewGameSpeed(scores.score);
             if (IsGameOver())
             {
                 gameOver = true;
@@ -154,39 +151,8 @@ namespace Tetris
                 GameSpeed -= 50;
             }
         }
-        public int bonusScore(int line)
-        {
-            int result = 0;
-            result = multipleLineBonus(line);
-            result = result == 0 ? 0 : combosTimeBonus(result);
-            return result;
-        }
-        public int multipleLineBonus(int line)
-        {
-            int result = 0;
-            for(int i = 0; i <= line; i++)
-            {
-                result += i;
-            }
-            return result;
-        }
-        public int combosTimeBonus(int result)
-        {
-            int currentTime = time;
-            int diff = currentTime - lastScoreTime;
-            if (diff <= 15 && time > 15)
-            {
-                if (combos < 10)
-                {
-                    combos++;
-                }
-            } else
-            {
-                combos = 1;
-            }
-            lastScoreTime = currentTime;
-            return result * combos;
-        }
+        
+        
         public void MoveDownTetramino()
         {
             currentTetramino.MoveTetramino(1, 0);
